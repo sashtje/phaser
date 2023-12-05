9d071ff796c667b54a68bcc1adfb73610b58806c
sashtje <sashtje.lefevre@gmail.com>
2023-12-06 01:50:53 +0300

    fix: fix paths

--- first-game/script.js
+++ first-game/script.js
@@ -39,11 +39,11 @@ function hitBomb(player, bomb) {
 }
 
 function preload() {
-  this.load.image("sky", "assets/sky.png");
-  this.load.image("ground", "assets/platform.png");
-  this.load.image("star", "assets/star.png");
-  this.load.image("bomb", "assets/bomb.png");
-  this.load.spritesheet("dude", "assets/dude.png", {
+  this.load.image("sky", "./assets/sky.png");
+  this.load.image("ground", "./assets/platform.png");
+  this.load.image("star", "./assets/star.png");
+  this.load.image("bomb", "./assets/bomb.png");
+  this.load.spritesheet("dude", "./assets/dude.png", {
     frameWidth: 32,
     frameHeight: 48,
   });
